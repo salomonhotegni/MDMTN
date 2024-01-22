62fb266e57988d865de2d3b5981acf02047f4595
Sedjro Salomon HOTEGNI <91737217+salomonhotegni@users.noreply.github.com>
2024-01-22 17:12:52 +0100

    Update Train_and_Test.py

--- Train_and_Test.py
+++ Train_and_Test.py
@@ -16,24 +16,30 @@ from src.utils.WCsAL_Test import test_multitask_model, test_single_model
 from Data_loaders.MultiMnistLoaders import MultiMnist_loaders
 from Data_loaders.Cifar10MnistLoaders import Cifar10Mnist_loaders
 
+from src.utils.KDMTL_Train import full_training_kdmtl
+from src.utils.KDMTL_Test import test_multitask_kdmtl_model
+
+from src.utils.MTAN_Train import full_training_mtan
+from src.utils.MTAN_Test import test_multitask_mtan_model
+
 def load_Cifar10Mnist_data():
     data_path = "Data/Cifar10Mnist"
     split_rate = 0.8
     batch_size = [256, 256] # (train, test)
 
-    train_transfprm = transforms.Compose([#transforms.Lambda(lambda x: Image.fromarray(np.uint8(x))),
+    train_transform = transforms.Compose([#transforms.Lambda(lambda x: Image.fromarray(np.uint8(x))),
                                         #transforms.RandomRotation(20),
                                         transforms.ToTensor(),
                                         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
                                         transforms.Lambda(lambda x: x.permute(0, 1, 2))
                                         ])
 
-    test_transfprm = transforms.Compose([transforms.ToTensor(),
+    test_transform = transforms.Compose([transforms.ToTensor(),
                                         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
                                         transforms.Lambda(lambda x: x.permute(0, 1, 2))
                                         ])
 
-    transformers = [train_transfprm, test_transfprm] # [None, None]
+    transformers = [train_transform, test_transform] # [None, None]
     train_loader, val_loader, test_loader = Cifar10Mnist_loaders(data_path, split_rate, transformers, batch_size)
     print("Data loaded!")
 
@@ -65,15 +71,8 @@ def train_and_test_model_CM(model, Cifar10mnist_params):
     train_loader, val_loader, test_loader = load_Cifar10Mnist_data()
 
     # # Choose device
-    # use_cuda = torch.cuda.is_available()
-    # device = torch.device("cuda" if use_cuda else "cpu")
-    # cudnn.benchmark = True
-    # if use_cuda == False:
-    #     print("WARNING: CPU will be used for training.")
-
-    Cifar10mnist_params["num_batchEpoch"] = len(train_loader)
-
-    print("Training...")
+    device = Cifar10mnist_params["device"]
+    print(f"Training... [--- running on {device} ---]")
     
     final_model, TR_metrics, ALL_TRAIN_LOSS, ALL_VAL_ACCU, ALL_ORIG_losses, MODEL_VAL_ACCU, BEST_val_accu, Best_iter = full_training(train_loader, val_loader, model,
                           Cifar10mnist_params, init_model = True)
@@ -90,6 +89,8 @@ def train_and_test_model_CM(model, Cifar10mnist_params):
 
     return Test_accuracy, prec_wrong_images, TR_metrics, ALL_TRAIN_LOSS, ALL_VAL_ACCU, ALL_ORIG_losses, MODEL_VAL_ACCU, BEST_val_accu, Best_iter
 
+
+#################################################################################
 
 def load_MultiMnist_data():
     data_path = "Data/MultiMnist"
@@ -122,6 +123,7 @@ def load_MultiMnist_data():
 
     return train_loader, val_loader, test_loader
 
+
 def train_and_test_model_MM(model, MultiMNISt_params):
 
     from time import time
@@ -130,18 +132,10 @@ def train_and_test_model_MM(model, MultiMNISt_params):
     print(datetime.datetime.now())
     t0 = time()
 
-    # # Choose device
-    # use_cuda = torch.cuda.is_available()
-    # device = torch.device("cuda" if use_cuda else "cpu")
-    # cudnn.benchmark = True
-    # if use_cuda == False:
-    #     print("WARNING: CPU will be used for training.")
-
     train_loader, val_loader, test_loader = load_MultiMnist_data()
-
-    MultiMNISt_params["num_batchEpoch"] = len(train_loader)
-
-    print("Training...")
+    
+    device = MultiMNISt_params["device"]
+    print(f"Training... [--- running on {device} ---]")
     
     final_model, TR_metrics, ALL_TRAIN_LOSS, ALL_VAL_ACCU, ALL_ORIG_losses, MODEL_VAL_ACCU, BEST_val_accu, Best_iter = full_training(train_loader, val_loader, model,
                           MultiMNISt_params, init_model = True)
@@ -158,11 +152,9 @@ def train_and_test_model_MM(model, MultiMNISt_params):
 
     return Test_accuracy, prec_wrong_images, TR_metrics, ALL_TRAIN_LOSS, ALL_VAL_ACCU, ALL_ORIG_losses, MODEL_VAL_ACCU, BEST_val_accu, Best_iter
 
-##########################
-######### STL ############
-##########################
+#################################################################################
 
-def train_and_test_STL_model_CM(model, Cifar10mnist_params_STL):
+def train_and_test_KDMTLmodel_CM(model, Cifar10mnist_params):
 
     from time import time
     # Start timer
@@ -171,10 +163,20 @@ def train_and_test_STL_model_CM(model, Cifar10mnist_params_STL):
     t0 = time()
 
     train_loader, val_loader, test_loader = load_Cifar10Mnist_data()
-
-    print("Training...")
     
-    final_model, ALL_TRAIN_LOSS, ALL_VAL_ACCU = train_single_model(train_loader, val_loader, model, Cifar10mnist_params_STL)
+    size_data_for_search = int(0.1 * len(train_loader.dataset))
+    _ = len(train_loader.dataset) - size_data_for_search
+    dataset_for_search, _ = torch.utils.data.random_split(train_loader.dataset, [size_data_for_search, _])
+    dataloader_for_search = torch.utils.data.DataLoader(dataset_for_search, batch_size=256, shuffle=True)
+
+
+    device = Cifar10mnist_params["device"]
+    print(f"Training... [-- running on {device} --]")
+    
+    Cifar10mnist_params["data_search"] = dataloader_for_search
+    
+    final_model, ALL_TRAIN_LOSS, ALL_VAL_ACCU, MODEL_VAL_ACCU = full_training_kdmtl(train_loader, val_loader, model,
+                          Cifar10mnist_params, init_model = True)
        
     print("Training completed !") 
 
@@ -184,13 +186,14 @@ def train_and_test_STL_model_CM(model, Cifar10mnist_params_STL):
     print(datetime.datetime.now())
 
     print("Testing ...") 
-    Test_accuracy, prec_wrong_images = test_single_model(test_loader, final_model, Cifar10mnist_params_STL) 
+    Test_accuracy, prec_wrong_images = test_multitask_kdmtl_model(test_loader, final_model, Cifar10mnist_params, load = True) 
 
-    return Test_accuracy, prec_wrong_images, ALL_TRAIN_LOSS, ALL_VAL_ACCU
+    return Test_accuracy, prec_wrong_images, ALL_TRAIN_LOSS, ALL_VAL_ACCU, MODEL_VAL_ACCU
+
+################################################################################
 
 
-
-def train_and_test_STL_model_MM(model, MultiMNISt_params_STL):
+def train_and_test_KDMTLmodel_MM(model, MultiMNISt_params):
 
     from time import time
     # Start timer
@@ -199,10 +202,57 @@ def train_and_test_STL_model_MM(model, MultiMNISt_params_STL):
     t0 = time()
 
     train_loader, val_loader, test_loader = load_MultiMnist_data()
-
-    print("Training...")
     
-    final_model, ALL_TRAIN_LOSS, ALL_VAL_ACCU = train_single_model(train_loader, val_loader, model, MultiMNISt_params_STL)
+    size_data_for_search = int(0.1 * len(train_loader.dataset))
+    _ = len(train_loader.dataset) - size_data_for_search
+    dataset_for_search, _ = torch.utils.data.random_split(train_loader.dataset, [size_data_for_search, _])
+    dataloader_for_search = torch.utils.data.DataLoader(dataset_for_search, batch_size=256, shuffle=True)
+    
+    device = MultiMNISt_params["device"]
+    print(f"Training... [-- running on {device} --]")
+    
+    MultiMNISt_params["data_search"] = dataloader_for_search
+    
+    final_model, ALL_TRAIN_LOSS, ALL_VAL_ACCU, MODEL_VAL_ACCU = full_training_kdmtl(train_loader, val_loader, model,
+                          MultiMNISt_params, init_model = True)
+    
+    print("Training completed !") 
+
+    T_norm_1 = time()-t0
+    # Print computation time
+    print('\nComputation time: {} minutes'.format(T_norm_1/60))
+    print(datetime.datetime.now())
+
+    print("Testing ...") 
+    Test_accuracy, prec_wrong_images = test_multitask_kdmtl_model(test_loader, final_model, MultiMNISt_params, load = True) 
+
+    return Test_accuracy, prec_wrong_images, ALL_TRAIN_LOSS, ALL_VAL_ACCU, MODEL_VAL_ACCU
+
+#################################################################################
+
+def train_and_test_MTANmodel_CM(model, Cifar10mnist_params):
+
+    from time import time
+    # Start timer
+    import datetime
+    print(datetime.datetime.now())
+    t0 = time()
+
+    train_loader, val_loader, test_loader = load_Cifar10Mnist_data()
+    
+    size_data_for_search = int(0.1 * len(train_loader.dataset))
+    _ = len(train_loader.dataset) - size_data_for_search
+    dataset_for_search, _ = torch.utils.data.random_split(train_loader.dataset, [size_data_for_search, _])
+    dataloader_for_search = torch.utils.data.DataLoader(dataset_for_search, batch_size=256, shuffle=True)
+
+
+    device = Cifar10mnist_params["device"]
+    print(f"Training... [-- running on {device} --]")
+    
+    Cifar10mnist_params["data_search"] = dataloader_for_search
+    
+    final_model, ALL_TRAIN_LOSS, ALL_VAL_ACCU, MODEL_VAL_ACCU = full_training_mtan(train_loader, val_loader, model,
+                          Cifar10mnist_params, init_model = True)
        
     print("Training completed !") 
 
@@ -212,6 +262,122 @@ def train_and_test_STL_model_MM(model, MultiMNISt_params_STL):
     print(datetime.datetime.now())
 
     print("Testing ...") 
-    Test_accuracy, prec_wrong_images = test_single_model(test_loader, final_model, MultiMNISt_params_STL) 
+    Test_accuracy, prec_wrong_images = test_multitask_mtan_model(test_loader, final_model, Cifar10mnist_params, load = True) 
+
+    return Test_accuracy, prec_wrong_images, ALL_TRAIN_LOSS, ALL_VAL_ACCU, MODEL_VAL_ACCU
+
+#################################################################################
+
+def train_and_test_MTANmodel_MM(model, MultiMNISt_params):
+
+    from time import time
+    # Start timer
+    import datetime
+    print(datetime.datetime.now())
+    t0 = time()
+
+    train_loader, val_loader, test_loader = load_MultiMnist_data()
+    
+    size_data_for_search = int(0.1 * len(train_loader.dataset))
+    _ = len(train_loader.dataset) - size_data_for_search
+    dataset_for_search, _ = torch.utils.data.random_split(train_loader.dataset, [size_data_for_search, _])
+    dataloader_for_search = torch.utils.data.DataLoader(dataset_for_search, batch_size=256, shuffle=True)
+    
+    device = MultiMNISt_params["device"]
+    print(f"Training... [-- running on {device} --]")
+    
+    MultiMNISt_params["data_search"] = dataloader_for_search
+    
+    final_model, ALL_TRAIN_LOSS, ALL_VAL_ACCU, MODEL_VAL_ACCU = full_training_mtan(train_loader, val_loader, model,
+                          MultiMNISt_params, init_model = True)
+    
+    print("Training completed !") 
+
+    T_norm_1 = time()-t0
+    # Print computation time
+    print('\nComputation time: {} minutes'.format(T_norm_1/60))
+    print(datetime.datetime.now())
+
+    print("Testing ...") 
+    Test_accuracy, prec_wrong_images = test_multitask_mtan_model(test_loader, final_model, MultiMNISt_params, load = True) 
+
+    return Test_accuracy, prec_wrong_images, ALL_TRAIN_LOSS, ALL_VAL_ACCU, MODEL_VAL_ACCU
+
+#################################################################################
+
+##########################
+######### STL ############
+##########################
+
+def train_and_test_STL_model_CM(model, Cifar10mnist_params_STL, train_func = None, test_func = None):
+
+    from time import time
+    # Start timer
+    import datetime
+    print(datetime.datetime.now())
+    t0 = time()
+
+    train_loader, val_loader, test_loader = load_Cifar10Mnist_data()
+    
+    print("Training ...") 
+    
+    if train_func is None:
+        train_func = train_single_model
+    
+    final_model, ALL_TRAIN_LOSS, ALL_VAL_ACCU = train_func(train_loader, val_loader, model, Cifar10mnist_params_STL)
+
+    print("Training completed !") 
+    
+
+    T_norm_1 = time()-t0
+    # Print computation time
+    print('\nComputation time: {} minutes'.format(T_norm_1/60))
+    print(datetime.datetime.now())
+
+    print("Testing ...") 
+    
+    if test_func is None:
+        test_func = test_single_model
+        
+    Test_accuracy, prec_wrong_images = test_func(test_loader, final_model, Cifar10mnist_params_STL) 
 
     return Test_accuracy, prec_wrong_images, ALL_TRAIN_LOSS, ALL_VAL_ACCU
+
+
+
+def train_and_test_STL_model_MM(model, MultiMNISt_params_STL, train_func = None, test_func = None):
+
+    from time import time
+    # Start timer
+    import datetime
+    print(datetime.datetime.now())
+    t0 = time()
+
+    train_loader, val_loader, test_loader = load_MultiMnist_data()
+    
+    print("Training ...") 
+    
+    if train_func is None:
+        train_func = train_single_model
+    
+    final_model, ALL_TRAIN_LOSS, ALL_VAL_ACCU = train_func(train_loader, val_loader, model, MultiMNISt_params_STL)
+
+    print("Training completed !") 
+    
+
+    T_norm_1 = time()-t0
+    # Print computation time
+    print('\nComputation time: {} minutes'.format(T_norm_1/60))
+    print(datetime.datetime.now())
+
+    print("Testing ...") 
+    
+    if test_func is None:
+        test_func = test_single_model
+        
+    Test_accuracy, prec_wrong_images = test_func(test_loader, final_model, MultiMNISt_params_STL) 
+
+    return Test_accuracy, prec_wrong_images, ALL_TRAIN_LOSS, ALL_VAL_ACCU
+
+###############################################
+
