97a4e52ec48467a639789283a7d030acb100c2f8
Sedjro Salomon HOTEGNI <91737217+salomonhotegni@users.noreply.github.com>
2024-01-24 02:03:17 +0100

    Update Train_and_Test.py

--- Train_and_Test.py
+++ Train_and_Test.py
@@ -1,26 +1,25 @@
-import numpy as np 
-import torch
-import torch.backends.cudnn as cudnn
-from torchvision import transforms
-from PIL import Image
+import warnings
 
 import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.backends.cudnn as cudnn
+from PIL import Image
+from torchvision import transforms
 
 #plt.style.use('ggplot')
 
-import warnings
 warnings.filterwarnings("ignore", category=FutureWarning)
 
-from src.utils.WCsAL_Train import full_training, train_single_model
-from src.utils.WCsAL_Test import test_multitask_model, test_single_model
-from Data_loaders.MultiMnistLoaders import MultiMnist_loaders
 from Data_loaders.Cifar10MnistLoaders import Cifar10Mnist_loaders
-
-from src.utils.KDMTL_Train import full_training_kdmtl
+from Data_loaders.MultiMnistLoaders import MultiMnist_loaders
 from src.utils.KDMTL_Test import test_multitask_kdmtl_model
-
-from src.utils.MTAN_Train import full_training_mtan
+from src.utils.KDMTL_Train import full_training_kdmtl
 from src.utils.MTAN_Test import test_multitask_mtan_model
+from src.utils.MTAN_Train import full_training_mtan
+from src.utils.WCsAL_Test import test_multitask_model, test_single_model
+from src.utils.WCsAL_Train import full_training, train_single_model
+
 
 def load_Cifar10Mnist_data():
     data_path = "Data/Cifar10Mnist"
@@ -62,9 +61,9 @@ def load_Cifar10Mnist_data():
 
 def train_and_test_model_CM(model, Cifar10mnist_params):
 
-    from time import time
     # Start timer
     import datetime
+    from time import time
     print(datetime.datetime.now())
     t0 = time()
 
@@ -126,9 +125,9 @@ def load_MultiMnist_data():
 
 def train_and_test_model_MM(model, MultiMNISt_params):
 
-    from time import time
     # Start timer
     import datetime
+    from time import time
     print(datetime.datetime.now())
     t0 = time()
 
@@ -158,7 +157,7 @@ def train_and_test_KDMTLmodel_CM(model, Cifar10mnist_params):
 
     train_loader, val_loader, test_loader = load_Cifar10Mnist_data()
     
-    size_data_for_search = int(0.1 * len(train_loader.dataset))
+    size_data_for_search = int(0.2 * len(train_loader.dataset))
     _ = len(train_loader.dataset) - size_data_for_search
     dataset_for_search, _ = torch.utils.data.random_split(train_loader.dataset, [size_data_for_search, _])
     dataloader_for_search = torch.utils.data.DataLoader(dataset_for_search, batch_size=256, shuffle=True)
@@ -239,7 +238,7 @@ def train_and_test_MTANmodel_MM(model, MultiMNISt_params):
 
     train_loader, val_loader, test_loader = load_MultiMnist_data()
     
-    size_data_for_search = int(0.1 * len(train_loader.dataset))
+    size_data_for_search = int(0.2 * len(train_loader.dataset))
     _ = len(train_loader.dataset) - size_data_for_search
     dataset_for_search, _ = torch.utils.data.random_split(train_loader.dataset, [size_data_for_search, _])
     dataloader_for_search = torch.utils.data.DataLoader(dataset_for_search, batch_size=256, shuffle=True)
@@ -252,9 +251,9 @@ def train_and_test_MTANmodel_MM(model, MultiMNISt_params):
     final_model, ALL_TRAIN_LOSS, ALL_VAL_ACCU, MODEL_VAL_ACCU = full_training_mtan(train_loader, val_loader, model,
                           MultiMNISt_params, init_model = True)
     
-    print("Training completed !") 
+    print("Training completed !")
 
-    print("Testing ...") 
+    print("Testing ...")
     Test_accuracy, prec_wrong_images = test_multitask_mtan_model(test_loader, final_model, MultiMNISt_params, load = True) 
 
     return Test_accuracy, prec_wrong_images, ALL_TRAIN_LOSS, ALL_VAL_ACCU, MODEL_VAL_ACCU
@@ -267,9 +266,9 @@ def train_and_test_MTANmodel_MM(model, MultiMNISt_params):
 
 def train_and_test_STL_model_CM(model, Cifar10mnist_params_STL, train_func = None, test_func = None):
 
-    from time import time
     # Start timer
     import datetime
+    from time import time
     print(datetime.datetime.now())
     t0 = time()
 
@@ -303,9 +302,9 @@ def train_and_test_STL_model_CM(model, Cifar10mnist_params_STL, train_func = Non
 
 def train_and_test_STL_model_MM(model, MultiMNISt_params_STL, train_func = None, test_func = None):
 
-    from time import time
     # Start timer
     import datetime
+    from time import time
     print(datetime.datetime.now())
     t0 = time()
 
